5bce24a1d9f56715937e054dc9b916a23f4b712c
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-08-03 18:40:14 -0300

    Add a new event type to reset a category

--- src/types/RoomEvent.ts
+++ src/types/RoomEvent.ts
@@ -1,6 +1,7 @@
 export type RoomEvent =
   | { type: "category_fixed"; slot: "a" | "b"; category: string }
   | { type: "category_completed"; slot: "a" | "b"; category: string }
+  | { type: "category_uncompleted"; slot: "a" | "b"; category: string }
   | { type: "progress_updated"; slot: "a" | "b"; progress: number }
   | { type: "is_ready"; slot: "a" | "b" }
   | { type: "say"; slot: "a" | "b"; message: string }
