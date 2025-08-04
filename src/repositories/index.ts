a7313ab7c722877eded018f7ce8952fd87473978
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-08-04 09:37:54 -0300

    Add index file to export repositories and entities

---
+++ src/repositories/index.ts
@@ -0,0 +1,9 @@
+export { ConnectionRepository } from "./ConnectionRepository";
+export { RoomRepository } from "./RoomRepository";
+export { RepositoryFactory } from "./RepositoryFactory";
+export {
+  createConnectionEntity,
+  ConnectionEntity,
+  ConnectionItem,
+} from "./entities/Connection";
+export { createRoomEntity, RoomEntity, RoomItem } from "./entities/Room";
