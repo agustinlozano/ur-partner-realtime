e444ab5d8856c77fff21f51c91d3302fd4298754
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-08-04 21:45:48 -0300

    Add new methods based on ElectroDB

--- src/services/ServiceFactory.ts
+++ src/services/ServiceFactory.ts
@@ -3,17 +3,45 @@ import { ApiGatewayManagementApiClient } from "@aws-sdk/client-apigatewaymanagem
 import { ConnectionService } from "@/services/ConnectionService";
 import { RoomService } from "@/services/RoomService";
 
+import { ConnectionServiceV2 } from "@/services/ConnectionServiceV2";
+import { RoomServiceV2 } from "@/services/RoomServiceV2";
+import { RepositoryFactory } from "@/repositories/RepositoryFactory";
+
 export class ServiceFactory {
+  private repositoryFactory: RepositoryFactory;
+
   constructor(
     private readonly dynamo: DynamoDBDocumentClient,
     private readonly api: ApiGatewayManagementApiClient
-  ) {}
+  ) {
+    this.repositoryFactory = new RepositoryFactory(dynamo);
+  }
 
-  createConnectionService() {
+  // @aws-sdk/dynamodb based
+  createConnectionServiceOld() {
     return new ConnectionService(this.dynamo);
   }
 
-  createRoomService() {
+  // @aws-sdk/dynamodb based
+  createRoomServiceOld() {
     return new RoomService(this.dynamo, this.api);
+  }
+
+  createConnectionService() {
+    const connectionRepository =
+      this.repositoryFactory.createConnectionRepository();
+    return new ConnectionServiceV2(connectionRepository);
+  }
+
+  createRoomService() {
+    const roomRepository = this.repositoryFactory.createRoomRepository();
+    const connectionRepository =
+      this.repositoryFactory.createConnectionRepository();
+    return new RoomServiceV2(roomRepository, connectionRepository, this.api);
+  }
+
+  // Direct repository access if needed
+  getRepositoryFactory() {
+    return this.repositoryFactory;
   }
 }
